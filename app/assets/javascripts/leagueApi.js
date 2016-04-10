72927e987b57716b5af95e1ef4eba314e1bf04dd
Arjun <arjundutta91@gmail.com>
2016-04-09 17:19:58 -0700

    pre-pull

--- app/assets/javascripts/leagueApi.js
+++ app/assets/javascripts/leagueApi.js
@@ -1,6 +1,6 @@
 $.ajax({
   // TODO: add key
-  url: "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/adutta?api_key=",
+  url: "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/adutta?api_key=bd9c0a96-b497-4619-adb8-1965df5bb4a7",
   method: "GET",
   success: function(data) {
     window.testValue = data;
