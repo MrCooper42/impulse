03881c44e314412b96cc02aecd3c5603944ac22e
Arjun <arjundutta91@gmail.com>
2016-04-21 17:22:56 -0700

    add quotation marks to quotes

--- app/assets/objects/quotes.js
+++ app/assets/objects/quotes.js
@@ -1,22 +1,22 @@
 var QUOTES = {
 
   quotes: [
-    "Do I make your pulse rise? - Ahri",
-    "Are you sure you can handle me, Summoner? - Miss Fortune",
-    "I will guide you. - Nidalee",
-    "On the prowl. - Nidalee",
-    "I'll show these house cats REAL claws! - Nidalee",
-    "You cannot cage me, Summoner. - Nidalee",
-    "My wings are restless! - Kha'Zix",
-    "Mundo too strong for you! - Dr. Mundo",
-    "A whole world to toy with. - Syndra",
-    "Ok. - Rammus",
-    "Who needs a map? - Ezreal",
-    "Lighting the way! - Lux",
-    "It's cold out there. - Nunu",
-    "What is broken can be reforged. - Riven",
-    "A broken blade is more than enough for the likes of you! - Riven",
-    "Time flies like an arrow, fruit flies like banana. - Zilean"
+    " '' Do I make your pulse rise? ''  - Ahri",
+    " '' Are you sure you can handle me, Summoner? ''  - Miss Fortune",
+    " '' I will guide you. ''  - Nidalee",
+    " '' On the prowl. ''  - Nidalee",
+    " '' I'll show these house cats REAL claws! ''  - Nidalee",
+    " '' You cannot cage me, Summoner. ''  - Nidalee",
+    " '' My wings are restless! ''  - Kha '' Zix",
+    " '' Mundo too strong for you! ''  - Dr. Mundo",
+    " '' A whole world to toy with. ''  - Syndra",
+    " '' Ok. ''  - Rammus",
+    " '' Who needs a map? ''  - Ezreal",
+    " '' Lighting the way! ''  - Lux",
+    " '' Its cold out there. ''  - Nunu",
+    " '' What is broken can be reforged. ''  - Riven",
+    " '' A broken blade is more than enough for the likes of you! ''  - Riven",
+    " '' Time flies like an arrow, fruit flies like banana. ''  - Zilean"
   ]
 
 };
