84da619bd6d43af57fe29972b04b608c435709a9
mattyshen <mattyshen@gmail.com>
2016-04-26 15:31:53 -0700

    progression getData fix

--- components/progression.jsx
+++ components/progression.jsx
@@ -11,12 +11,21 @@ var Progression = React.createClass({
   getInitialState: function(){
     return {
       recentGame: GameStore.lastGame(),
-      otherGames: GameStore.allGames().slice(1)
+      otherGames: GameStore.allGames().slice(1),
+      allGames: GameStore.allGames()
     };
   },
 
   componentDidMount: function(){
     this.gameListener = GameStore.addListener(this.update);
+    if (localStorage['leagueGames']) {
+    var games = JSON.parse(localStorage['leagueGames']);
+    this.setState({
+      recentGame: games.length === 0 ? {} : games[0],
+      otherGames: games.slice(1),
+      allGames: games
+    });
+  }
   },
 
   componentWillUnmount: function(){
@@ -26,9 +35,11 @@ var Progression = React.createClass({
   update: function(){
     this.setState({
       recentGame: GameStore.lastGame(),
-      otherGames: GameStore.allGames().slice(1)
+      otherGames: GameStore.allGames().slice(1),
+      allGames: GameStore.allGames()
     })
   },
+
 
   calcAverages: function(){
     var killSum = 0;
@@ -52,6 +63,7 @@ var Progression = React.createClass({
       gameTimeSum = stats.timePlayed ? gameTimeSum + stats.timePlayed : gameTimeSum;
     });
     var avgGameTimeMin = Math.floor((gameTimeSum/numGames)/60);
+
     return {
       killAvg: (killSum/numGames).toFixed(2),
       deathAvg: (deathSum/numGames).toFixed(2),
@@ -65,24 +77,25 @@ var Progression = React.createClass({
   generateStats: function() {
     if (this.state.recentGame.stats) {
       var gameStats = this.state.recentGame.stats;
-      var averages = this.calcAverages();
       var playTime = gameStats.timePlayed/60;
+      var averages = this.calcAverages();
+      var displayStats = {}
 
-      gameStats.championsKilled = gameStats.championsKilled ? gameStats.championsKilled : 0;
-      gameStats.numDeaths = gameStats.numDeaths ? gameStats.numDeaths : 0;
-      gameStats.assists = gameStats.assists ? gameStats.assists : 0;
-      gameStats.minionsKilled = gameStats.minionsKilled ? (gameStats.minionsKilled/playTime).toFixed(2) : 0;
-      gameStats.totalDamageDealtToChampions = gameStats.totalDamageDealtToChampions ? (gameStats.totalDamageDealtToChampions/playTime).toFixed(2) : 0;
-
+      displayStats.championsKilled = gameStats.championsKilled ? gameStats.championsKilled : 0;
+      displayStats.numDeaths = gameStats.numDeaths ? gameStats.numDeaths : 0;
+      displayStats.assists = gameStats.assists ? gameStats.assists : 0;
+      displayStats.goldEarned = gameStats.goldEarned ? (gameStats.goldEarned/playTime).toFixed(2) : 0;
+      displayStats.minionsKilled = gameStats.minionsKilled ? (gameStats.minionsKilled/playTime).toFixed(2) : 0;
+      displayStats.totalDamageDealtToChampions = gameStats.totalDamageDealtToChampions ? (gameStats.totalDamageDealtToChampions/playTime).toFixed(2) : 0;
 
       return (
         <ul>
-           <li>Kills: {gameStats.championsKilled} --> {averages.killAvg}</li>
-           <li>Deaths: {gameStats.numDeaths} --> {averages.deathAvg}</li>
-           <li>Assists: {gameStats.assists} --> {averages.assistAvg}</li>
-           <li>Gold/min: {(gameStats.goldEarned/playTime).toFixed(2)} --> {averages.goldAvg}</li>
-           <li>CS/min: {gameStats.minionsKilled} --> {averages.minionAvg}</li>
-           <li>Dmg/min: {gameStats.totalDamageDealtToChampions} --> {averages.damageAvg}</li>
+           <li>Kills: {displayStats.championsKilled} {this.killCompare(averages)} ({averages.killAvg})</li>
+           <li>Deaths: {displayStats.numDeaths} {this.deathCompare(averages)} ({averages.deathAvg})</li>
+           <li>Assists: {displayStats.assists} {this.assistCompare(averages)} ({averages.assistAvg})</li>
+           <li>Gold/min: {displayStats.goldEarned} {this.goldCompare(averages)} ({averages.goldAvg})</li>
+           <li>CS/min: {displayStats.minionsKilled} {this.CSCompare(averages)} ({averages.minionAvg})</li>
+           <li>Dmg/min: {displayStats.totalDamageDealtToChampions} {this.dmgCompare(averages)} ({averages.damageAvg})</li>
         </ul>
       )
     } else {
@@ -92,16 +105,83 @@ var Progression = React.createClass({
     }
   },
 
+  killCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    if(gameStats.championsKilled < averages.killAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  deathCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    if(gameStats.numDeaths < averages.deathAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  assistCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    if(gameStats.assists < averages.assistAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  goldCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    var playTime = gameStats.timePlayed/60;
+    if(gameStats.goldEarned/playTime < averages.goldAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  CSCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    var playTime = gameStats.timePlayed/60;
+    if(gameStats.minionsKilled/playTime < averages.minionAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  dmgCompare: function(averages){
+    var gameStats = this.state.recentGame.stats;
+    var playTime = gameStats.timePlayed/60;
+    if(gameStats.totalDamageDealtToChampions/playTime < averages.damageAvg){
+      return "↓";
+    } else {
+      return "↑";
+    }
+  },
+
+  getData: function(){
+
+    if (this.state.allGames) {
+      
+      
+      console.log(this.state.allGames)
+
+    } else {
+      console.log("nope")
+    }
+
+    return [
+            [{x: '1-Jan-15', y: 20}, {x: '1-Feb-15', y: 10}, {x: '1-Mar-15', y: 33}, {x: '1-Apr-15', y: 45}, {x: '1-May-15', y: 15}],
+
+            [{x: '1-Jan-15', y: 10}, {x: '1-Feb-15', y: 15}, {x: '1-Mar-15', y: 13}, {x: '1-Apr-15', y: 15}, {x: '1-May-15', y: 10}]
+    ];
+  },
 
   render: function(){
-        var lineData = [
-          {
-            name: "series1",
-            values: [ { x: 0, y: 20 }, { x: 24, y: 10 } ],
-            strokeWidth: 3,
-            strokeDashArray: "5,5"
-          }
-        ];
+
 
     return (
       <div className="progression">
@@ -113,13 +193,10 @@ var Progression = React.createClass({
           grid
           verticalGrid
           interpolate={'cardinal'}
-          lineColors={['yellow', 'cyan']}
+          lineColors={['red', 'blue']}
           width={750}
           height={250}
-          data={[
-            [{x: '1-Jan-15', y: 20}, {x: '1-Feb-15', y: 10}, {x: '1-Mar-15', y: 33}, {x: '1-Apr-15', y: 45}, {x: '1-May-15', y: 15}],
-            [{x: '1-Jan-15', y: 10}, {x: '1-Feb-15', y: 15}, {x: '1-Mar-15', y: 13}, {x: '1-Apr-15', y: 15}, {x: '1-May-15', y: 10}]
-          ]}
+          data={this.getData()}
         />
         
       
---
+++ components/settings.jsx
@@ -0,0 +1,66 @@
+var React = require('react');
+var SettingStore = require('../stores/settingStore');
+var SettingsUtil = require('../util/settingsUtil');
+
+var WIDGETS = require('../app/assets/objects/widgetList');
+var DEFAULTS = require('../app/assets/objects/defaultSettings');
+
+var Settings = React.createClass({
+
+  getInitialState: function() {
+    return ({
+      settings: SettingStore.settings()
+    });
+  },
+
+  componentDidMount: function() {
+    this.settingsListener = SettingStore.addListener(this.update);
+    if (localStorage['widgetSettings']) {
+      SettingsUtil.setSettings(JSON.parse(localStorage['widgetSettings']));
+    }
+  },
+
+  componentWillUnmount: function() {
+    this.settingsListener.remove();
+  },
+
+  update: function() {
+    this.setState({ settings: SettingStore.settings() });
+  },
+
+  changeSettings: function(event) {
+    event.preventDefault();
+    var widget = event.currentTarget.id;
+    var newSettings = this.state.settings;
+    newSettings[widget] = !newSettings[widget];
+    SettingsUtil.setSettings(newSettings);
+  },
+
+  displaySettings: function() {
+    that = this
+    return Object.keys(this.state.settings).map(function(widget, idx) {
+      var toggled = that.state.settings[widget] ? "settingsButtonOn" : "settingsButtonOff"
+      return (
+        <li key={idx}>
+          <div>
+            <span>{widget}</span>
+            <button onClick={that.changeSettings} id={widget} className={toggled} value="toggle"/>
+          </div>
+        </li>
+      );
+    });
+  },
+
+  render: function() {
+    return (
+      <div className="settingsMenu">
+        <ul>
+          {this.displaySettings()}
+        </ul>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Settings;
