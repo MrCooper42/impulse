65d5f87c41e5f7119f1ed47d5006a6260a62a691
Arjun <arjundutta91@gmail.com>
2016-04-28 13:52:09 -0700

    add item lists

--- components/games/recentGameInfo.jsx
+++ components/games/recentGameInfo.jsx
@@ -8,9 +8,12 @@ var GameStore = require('../../stores/gameStore');
 
 // OBJECTS
 var CHAMPIONS = require('../../app/assets/objects/championsMap');
+var GAME_MODES = require('../../app/assets/objects/gameModes');
+
+// IMAGE URLS
 var CHAMP_SQUARES = require('../../app/assets/images/squares');
 var SUMMONER_SPELLS = require('../../app/assets/images/summonerSpells');
-var GAME_MODES = require('../../app/assets/objects/gameModes');
+var ITEMS = require('../../app/assets/images/items');
 
 
 var RecentGameInfo = React.createClass({
@@ -51,15 +54,25 @@ var RecentGameInfo = React.createClass({
       }
       stats = (
         <div className="recentGameInfo">
+          <h2>{gameStats.win ? "VICTORY" : "DEFEAT"}</h2>
+          <div className="statsName">{CHAMPIONS[this.state.game.championId]}</div>
+          <div className="statsCreate">
+            {GAME_MODES[this.state.game.gameMode]} {GAME_MODES[this.state.game.gameType]} {subType}
+          </div>
           <div>
             <img src={SUMMONER_SPELLS[this.state.game.spell1].url} className="spell"/>
             <img src={SUMMONER_SPELLS[this.state.game.spell2].url} className="spell"/>
           </div>
-          <div className="statsName">{CHAMPIONS[this.state.game.championId]}</div>
-          <div className="statsCreate">{date.toString().slice(3,15)}</div>
-          <div className="statsCreate">
-            {GAME_MODES[this.state.game.gameMode]} {GAME_MODES[this.state.game.gameType]} {subType}
+          <div className="itemsList">
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["default"]} className="item"/>
+            <img src={ITEMS["trinket-default"]} className="item"/>
           </div>
+          <div className="statsCreate">{date.toString().slice(3,15)}</div>
         </div>
       )
     }
