0b35ae50666b7fdf9553a66f6c14a575d28e193a
Arjun <arjundutta91@gmail.com>
2016-04-28 14:25:38 -0700

    fix summary stats on recentGame info

--- components/games/recentGameInfo.jsx
+++ components/games/recentGameInfo.jsx
@@ -14,6 +14,7 @@ var GAME_MODES = require('../../app/assets/objects/gameModes');
 var CHAMP_SQUARES = require('../../app/assets/images/squares');
 var SUMMONER_SPELLS = require('../../app/assets/images/summonerSpells');
 var ITEMS = require('../../app/assets/images/items');
+var ICONS = require('../../app/assets/images/icons');
 
 
 var RecentGameInfo = React.createClass({
@@ -55,28 +56,64 @@ var RecentGameInfo = React.createClass({
       stats = (
         <div className="recentGameInfo">
           <h2>{gameStats.win ? "VICTORY" : "DEFEAT"}</h2>
-          <div className="statsName">{CHAMPIONS[this.state.game.championId]}</div>
           <div className="statsCreate">
             {GAME_MODES[this.state.game.gameMode]} {GAME_MODES[this.state.game.gameType]} {subType}
           </div>
-          <div>
-            <img src={SUMMONER_SPELLS[this.state.game.spell1].url} className="spell"/>
-            <img src={SUMMONER_SPELLS[this.state.game.spell2].url} className="spell"/>
-          </div>
-          <div className="itemsList">
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["default"]} className="item"/>
-            <img src={ITEMS["trinket-default"]} className="item"/>
-          </div>
+          {this.getSummaryStats()}
+          {this.getSpells()}
+          {this.getItems()}
           <div className="statsCreate">{date.toString().slice(3,15)}</div>
         </div>
       )
     }
     return stats;
+  },
+
+  getSummaryStats: function() {
+    var gameStats = this.state.game.stats;
+    var timeParts = (gameStats.timePlayed/60).toFixed(2).split('.')
+    timeParts[1] = (Math.round(60/parseInt(timeParts[1])) * 10).toString();
+    var time = timeParts.join(":");
+    return (
+      <div className="gameStatsSummary">
+        <div className="statModule"><img src={ICONS["gold"]} className="icon"/>{this.parseNumber(gameStats.goldEarned)}</div>
+        <div className="statModule"><img src={ICONS["minion"]} className="icon"/>{gameStats.minionsKilled}</div>
+        <div className="statModule"><img src={ICONS["clock"]} className="icon"/>{time}</div>
+      </div>
+    );
+  },
+
+  parseNumber: function(num) {
+    var numString = num.toString().split('');
+    var i = numString.length - 3;
+    while (i > 0) {
+       numString.splice(i, 0, ",");
+       i -= 3;
+    }
+    return numString.join('');
+  },
+
+  getSpells: function() {
+    return (
+      <div>
+        <img src={SUMMONER_SPELLS[this.state.game.spell1].url} className="icon"/>
+        <img src={SUMMONER_SPELLS[this.state.game.spell2].url} className="icon"/>
+      </div>
+    );
+  },
+
+  getItems: function() {
+    return (
+      <div className="itemsList">
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["default"]} className="icon"/>
+        <img src={ITEMS["trinket-default"]} className="icon"/>
+      </div>
+    )
   },
 
   render: function() {
