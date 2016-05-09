907b7ec1e2276c1015d1ee8dcef05dd5dbddb4a0
Arjun <arjundutta91@gmail.com>
2016-05-09 12:37:03 -0700

    reflect info on currentGame

--- components/games/currentPlayerInfo.jsx
+++ components/games/currentPlayerInfo.jsx
@@ -14,16 +14,38 @@ var CurrentPlayerInfo = React.createClass({
     LeagueUtil.fetchSummonerInfoWithCallback(this.props.summoner.summonerId, this.props.modalCallback);
   },
 
+  getInfo: function() {
+    var summoner = this.props.summoner;
+    if (summoner.teamId === 100) {
+      return (
+        <div onClick={this.searchSummoner} key={summoner.summonerId} className={summoner.teamId === 100 ? "blueTeam" : "redTeam"}>
+          <h2>{summoner.summonerName}</h2>
+          <div><img className="currentGameImg" src={CHAMPION_SQUARES[CHAMPIONS[summoner.championId]]}/></div>
+          <div className="currentGameSpells">
+            <img className="icon" src={SUMMONER_SPELLS[summoner.spell1Id].url}/>
+            <img className="icon" src={SUMMONER_SPELLS[summoner.spell2Id].url}/>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div onClick={this.searchSummoner} key={summoner.summonerId} className={summoner.teamId === 100 ? "blueTeam" : "redTeam"}>
+          <div className="currentGameSpells">
+            <img className="icon" src={SUMMONER_SPELLS[summoner.spell1Id].url}/>
+            <img className="icon" src={SUMMONER_SPELLS[summoner.spell2Id].url}/>
+          </div>
+          <div><img className="currentGameImg" src={CHAMPION_SQUARES[CHAMPIONS[summoner.championId]]}/></div>
+          <h2>{summoner.summonerName}</h2>
+        </div>
+      )
+    }
+  },
+
   render: function() {
     var summoner = this.props.summoner;
     return (
-      <div onClick={this.searchSummoner} key={summoner.summonerId} className={summoner.teamId === 100 ? "blueTeam" : "redTeam"}>
-        <h2>{summoner.summonerName}</h2>
-        <div><img className="currentGameImg" src={CHAMPION_SQUARES[CHAMPIONS[summoner.championId]]}/></div>
-        <div className="currentGameSpells">
-          <img className="icon" src={SUMMONER_SPELLS[summoner.spell1Id].url}/>
-          <img className="icon" src={SUMMONER_SPELLS[summoner.spell2Id].url}/>
-        </div>
+      <div>
+        {this.getInfo()}
       </div>
     );
   }
